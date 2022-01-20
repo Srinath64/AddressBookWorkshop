f93550b18c20f76aa9ffc087c3b08cb7f91fa5e8
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-20 09:16:10 +0530

    Added : Contacts in AddressBook

---
+++ src/main/java/com/addressbook/Contact.java
@@ -0,0 +1,14 @@
+package com.addressbook;
+
+public class Contact {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    long phone;
+    String email;
+
+}
