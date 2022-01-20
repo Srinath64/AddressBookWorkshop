a72298b407828dad844a7e147a9ac997d55c1ec6
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-20 09:22:06 +0530

    Added : Ability to add new contact to AddressBook

---
+++ src/main/java/com/addressbook/AddressBook.java
@@ -0,0 +1,42 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+
+    // Method to create an object of Contact class with user input
+    public void addContact() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" Please enter the first name: ");
+        String firstName = scan.next();
+
+        System.out.print(" Please enter the last name: ");
+        String lastName = scan.next();
+
+        System.out.print(" Please enter the address: ");
+        String address = scan.next();
+
+        System.out.print(" Please enter the city: ");
+        String city = scan.next();
+
+        System.out.print(" Please enter the state: ");
+        String state = scan.next();
+
+        System.out.print(" Please enter the zip: ");
+        int zip = scan.nextInt();
+
+        System.out.print(" Please enter the phone number: ");
+        long phone = scan.nextLong();
+
+        System.out.print(" Please enter the email: ");
+        String email = scan.next();
+
+        // Creating a new object of the contact class with the parameter values filled by user
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+
+
+    }
+}
+
